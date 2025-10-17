33d85229bf0f426d1972ad1cebd805ba684dc595
MDeak97 <michaeldeyak90@gmail.com>
2025-10-17 14:56:36 +1100

    Answering Q4.2

--- script.js
+++ script.js
@@ -15,5 +15,25 @@ class Movie {
     this.rating = rating;
   }
 }
-
+// Display Movie class in the console
 console.log(Movie);
+
+// Q4.2 - Create an array of at least 10 movies with mock values and print the array in console.
+
+// Creating array with 10 Movie objects
+const movieArray = [
+  new Movie(5, "Inception", 2010, "PG-13"),
+  new Movie(1, "Matrix", 2010, "R"),
+  new Movie(8, "Interstellar", 2014, "PG-13"),
+  new Movie(3, "Avatar", 2009, "PG-13"),
+  new Movie(10, "Gladiator", 2000, "R"),
+  new Movie(7, "Titanic", 1997, "PG-13"),
+  new Movie(2, "The Dark Knight", 2008, "PG-13"),
+  new Movie(9, "Joker", 2019, "R"),
+  new Movie(4, "The Lion King", 1994, "G"),
+  new Movie(6, "Avengers: Endgame", 2019, "PG-13"),
+];
+
+// Print the entire array to the console.
+console.log("All Movies (Unsorted): ");
+console.log(movieArray);
